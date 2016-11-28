6c09c00f416dfb8ba22faf8e44fec6f2a28be7d9
windinhishair <xibaiyu@msn.com>
2016-11-28 18:17:30 +0800

    add server interface to load between

--- daily_tools/daily_download_tools.py
+++ daily_tools/daily_download_tools.py
@@ -8,12 +8,17 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-f', '--filename', help='File to download' )
 	parser.add_argument('-d', '--directory', help='directory to download')
+	parser.add_argument('-s', '--server', help='server', default=None)
+	parser.add_argument('-u', '--user', help='user', default=None)
 
 	args = parser.parse_args()
 
 	directory = args.directory
 	filename = args.filename
 
+	if args.server and args.user:
+
+		PATH= '%s@%s:/home/%s/' %(user, server, user)
 	if directory:
 		path = PATH + directory
 		command = 'scp -r -P %s %s ./' %(PORT, path)
