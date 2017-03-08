7bebc49508908fc773fc1f58b380c64d4757342a
windinhishair <xibaiyu@msn.com>
2017-03-08 13:16:53 +0800

    generate interface for analyz log file

--- daily_tools/log_analyze/log_parser.py
+++ daily_tools/log_analyze/log_parser.py
@@ -4,23 +4,53 @@
 
 import re
 import json
+import urlparse
+import os
 
+from datetime import datetime
 
 class LogParser():
-    def __init__(self, file_name):
-        self.file=file_name
+    def __init__(self):
+        pass
 
-    def __test_file(self):
-        raise NotImplementedError
+    def _test_file(self):
+        return os.path.exists(os.path.abspath(file_name))
 
-    def initial_target_pattern(self, keys_words):
-        raise NotImplementedError
+    def test_line(self, line, pattern_str):
+        try:
+            return re.search(re.compile(pattern_str), line)
+        except:
+            return False
 
-    def extract_target_log_records(self):
-        raise NotImplementedError
+    def get_string_from_records(self, str, head_str, tail_str, head_included=True, tail_included=True):
 
-    def get_statistic(self):
-        raise NotImplementedError
+        head = str.index(head_str)
+        tail = str.index(tail_str)
+        head_index = head if head_included else head + len(head_str)
+        tail_index = tail + len(tail_str) if tail_included else tail 
+        target = str[head_index:tail_index]
+        return target
 
+    def get_json_object(self, raw_str, head, tail):
+        json_str = self.get_string_from_records(raw_str, head, tail, head_included=True, tail_included=True)
+        # import ipdb; ipdb.set_trace()
+        return json.loads(json_str), raw_str.index(tail)
 
+    def get_time_stamp(self, time_str, time_pattern):
 
+        time_stampe = datetime.strptime(time_str, time_pattern)
+        return time_stampe
+
+    def _get_all_index(self, str, target_str):
+        return [each.start() for each in re.finditer(target_str, str)]
+
+    def _get_parsed_url(self, url, target='hostname'):
+        url_obj = urlparse.urlparse(url)
+        if target== 'hostname':
+            return url_obj.hostname
+        elif target == 'path':
+            return url_obj.path
+        elif target == 'port':
+            return url_obj.port
+        else:
+            return url_obj
